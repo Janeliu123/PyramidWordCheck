96cafee630a63777899111c0a853c4618246bdc5
Janeliu123 <jiajliu6666@gmail.com>
2020-09-24 14:11:43 -0500

    Build the Maven Spring Boot project

---
+++ src/main/java/fetchrewords/example/checkpyramidword/CheckPyramidWordApplication.java
@@ -0,0 +1,13 @@
+package fetchrewords.example.checkpyramidword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckPyramidWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckPyramidWordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fetchrewords/example/checkpyramidword/CheckPyramidWordApplicationTests.java
@@ -0,0 +1,16 @@
+package fetchrewords.example.checkpyramidword;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CheckPyramidWordApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
